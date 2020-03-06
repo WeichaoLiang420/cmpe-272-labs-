df73e5dd8b8e4a2a11fa370dd61ec41f2da56d8d
weichaoliang <weichaoleung@gmail.com>
2020-03-06 15:34:54 -0800

    ..

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require "header.php"
+?>
+
+
+<main>
+
+    <?php
+if (isset($_SESSION['user_Name'])) {
+    echo '<p class = "login-status"> You are logging!</p>';
+} else {
+
+    echo '<p class = "login-status"> Welcome to Weichao Liang website</p>';
+}
+?>
+</main>
+
+<?php
+// require "footer.php"
+?>
---
+++ news.php
@@ -0,0 +1,9 @@
+<?php
+require "header.php"
+?>
+
+<main>
+    <h1>
+    Recent no NEWS.
+    </h1>
+</main>
