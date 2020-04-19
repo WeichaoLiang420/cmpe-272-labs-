d1b2aa0b1950e9a945b0a163d73e3326cf13031f
weichaoliang <weichaoleung@gmail.com>
2020-04-19 15:49:45 -0700

    try

--- allusers.php
+++ allusers.php
@@ -1,29 +1,47 @@
 <?php
 require "head.php"
 ?>
+<section class="main-section alabaster" id="users">
+	<div class="container fullsize">
+    <table  >
+        <tr>
+            
+            <td class="tableheader"><strong>First Name</strong></td>
+            <td class="tableheader"><strong>Last Name</strong></td>
+            <td class="tableheader"><strong>Email</strong></td>
+            <td class="tableheader"><strong>Home Address</strong></td>
+            <td class="tableheader"><strong>Home Phone</strong></td>
+            <td class="tableheader"><strong>Cell Phone</strong></td>
+            
+        </tr>
+       
 <?php
-/**
-* Initialize the cURL session
-*/
+// 1. 初始化一个cURL会话
 $ch = curl_init();
 
-/**
-* Set the URL of the page or file to download.
-*/
+// 2. 设置请求选项, 包括具体的url
 curl_setopt($ch, CURLOPT_URL, "http://ruiyang90.info/users.php");
-
-/**
-* Ask cURL to return the contents in a variable instead of simply echoing them to the browser.
-*/
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HEADER, 0);
 
-/**
-* Execute the cURL session
-*/
-$contents = curl_exec ($ch);
+// 3. 执行一个cURL会话并且获取相关回复
+$response = curl_exec($ch);
 
-/**
-* Close cURL session
-*/
-curl_close ($ch);
+// if ($response  === FALSE) {
+//   echo "cURL 具体出错信息: " . curl_error($ch);
+// }
+
+// curl_exec($ch);
+$curl_info= curl_getinfo($ch);
+// 4. 释放cURL句柄,关闭一个cURL会话
+curl_close($ch)
+
+
+
 ?>
+      </table>
+    <br/>
+    <br/>
+    
+        </div>
+</section>
