6482a3b4f8bd72bb0243c650faac2bd4b0fcc54a
weichaoliang <weichaoleung@gmail.com>
2020-04-21 11:56:08 -0700

    try data

--- allusers.php
+++ allusers.php
@@ -4,10 +4,75 @@ require "head.php"
 
        
 <?php
+<section class="main-section alabaster" id="users">
+<div class="container fullsize">
+<table  >
+    <tr>
+        
+        <td class="tableheader"><strong>First Name</strong></td>
+        <td class="tableheader"><strong>Last Name</strong></td>
+        <td class="tableheader"><strong>Email</strong></td>
+        <td class="tableheader"><strong>Home Address</strong></td>
+        <td class="tableheader"><strong>Home Phone</strong></td>
+        <td class="tableheader"><strong>Cell Phone</strong></td>
+        
+    </tr>
+    <?php
+     require('mm.php');   
+     $sql = "SELECT * FROM GuestInfo";
+        $result = $mysqli->query($sql);      
+    while($row = $result -> fetch_assoc()){
+        echo "<tr>";
+            echo "<td>";
+                echo $row["first_name"];
+            echo "</td>";
+            echo "<td>";
+                echo $row["last_name"];
+            echo "</td>";
+            echo "<td>";
+                echo $row["email"];
+            echo "</td>";
+            echo "<td>";
+                echo $row["home_address"];
+            echo "</td>";
+            echo "<td>";
+                echo $row["home_phone"];
+            echo "</td>";
+            echo "<td>";
+                echo $row["cell_phone"];
+            echo "</td>";
+        echo "</tr>";
+    }
+?>
+        </table>
+<br/>
+<br/>
+
+    </div>
+</section>
+
+
+
+
+
 $curl = curl_init();
+
 curl_setopt($curl, CURLOPT_URL, 'http://ruiyang90.info/users.php');
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+//curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+$fp = fopen("yangrui.txt", "w");
 $output = curl_exec ( $curl );
+
+
+$output=str_ireplace('<header>','',$output);
+$output=str_ireplace('</header>','',$output);
+//$output_1 = strip_tags($output,'<head><meta>');
+//$output_1 = $out = preg_replace("/(?<=^|>).*?(?=<|$)/s", "", $output);
+echo $output;
+fwrite($fp , $output);
+fclose($fp);
+
 
 // curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 // curl_setopt($curl, CURLOPT_HEADER, 0);
